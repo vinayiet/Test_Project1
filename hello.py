9180de45a785396a07579855dd8001ff5329c1b7
Vinay Sharma <vinayiet435@gmail.com>
2026-04-30 10:16:17 +0530

    added new line to the hello

--- hello.py
+++ hello.py
@@ -1,8 +1,10 @@
 # This is my new file 
 print('Hello')
 
+print('this is new print statement')
 
-
-# git add hello.py
-# git commit -m "added new python file"
-# git push -u origin main
+# 1. Create a file (make changes into that file)
+# 2. git add file_name 
+# 3. git commit -m "Written the frontend code"
+# 4. git branch -> ? -> Main -> git push -u origin Main
+# main -> git push -u origin main
