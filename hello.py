31f22772af266bb746d96e881675a485f49a1a86
Vinay Sharma <vinayiet435@gmail.com>
2026-05-01 10:02:16 +0530

    new line added

--- hello.py
+++ hello.py
@@ -3,9 +3,30 @@ print('Hello')
 b = 45 
 print('this is new print statement')
 a = 20 
-
+z = 'new_var'
 # 1. Create a file (make changes into that file)
 # 2. git add file_name 
 # 3. git commit -m "Written the frontend code"
 # 4. git branch -> ? -> Main -> git push -u origin Main
 # main -> git push -u origin main
+
+
+
+# Whenever working on the collaborative project you must have to pulll 
+# the update code first....
+# 1. git pull origin main 
+# 2. whatever the change could be into the file 
+# incoming change -> remote changes accept (Another person's code)
+# current change -> when you want to commit your current 
+# both changes -> you want to keep both of the changes 
+# 3. git add filename 
+# 4. git commit -m "message"
+# 5. git push -u origin main 
+
+# 1.
+# Make a new repository...then push some code 
+# Make changes into thee repository and pull that updated code 
+# into your pc..
+# Make changes into the current code and the push it on the git hub 
+
+# Branching 
