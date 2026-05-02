cc9d217a4b19a3fea13ec069735416cd28010e80
Vinay Sharma <vinayiet435@gmail.com>
2026-05-02 09:50:40 +0530

    new feature

--- hello.py
+++ hello.py
@@ -1,33 +1,41 @@
-# This is my new file 
-print('Hello')
-b = 45 
-print('this is new print statement')
-a = 20 
-z = 'new_var'
-y = 'new_var1'
-# 1. Create a file (make changes into that file)
-# 2. git add file_name 
-# 3. git commit -m "Written the frontend code"
-# 4. git branch -> ? -> Main -> git push -u origin Main
-# main -> git push -u origin main
+# # This is my new file 
+# print('Hello')
+# b = 45 
+# print('this is new print statement')
+# a = 20 
+# z = 'new_var'
+# y = 'new_var1'
+# # 1. Create a file (make changes into that file)
+# # 2. git add file_name 
+# # 3. git commit -m "Written the frontend code"
+# # 4. git branch -> ? -> Main -> git push -u origin Main
+# # main -> git push -u origin main
 
 
 
-# Whenever working on the collaborative project you must have to pulll 
-# the update code first....
-# 1. git pull origin main 
-# 2. whatever the change could be into the file 
-# incoming change -> remote changes accept (Another person's code)
-# current change -> when you want to commit your current 
-# both changes -> you want to keep both of the changes 
-# 3. git add filename 
-# 4. git commit -m "message"
-# 5. git push -u origin main 
+# # Whenever working on the collaborative project you must have to pulll 
+# # the update code first....
+# # 1. git pull origin main 
+# # 2. whatever the change could be into the file 
+# # incoming change -> remote changes accept (Another person's code)
+# # current change -> when you want to commit your current 
+# # both changes -> you want to keep both of the changes 
+# # 3. git add filename 
+# # 4. git commit -m "message"
+# # 5. git push -u origin main 
 
-# 1.
-# Make a new repository...then push some code 
-# Make changes into thee repository and pull that updated code 
-# into your pc..
-# Make changes into the current code and the push it on the git hub 
+# # 1.
+# # Make a new repository...then push some code 
+# # Make changes into thee repository and pull that updated code 
+# # into your pc..
+# # Make changes into the current code and the push it on the git hub 
 
-# Branching 
+# # Branching 
+
+
+
+
+# # Task -> collaborator 
+# # Create a new branch and then make changes into that branch and 
+# # push that changes into the new feature branch and again merge that 
+# # branch to the main branch...
